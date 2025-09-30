fbebca792e7ca68e11c97e0e2499b1096410ab03
IonE <thebellepoque@gmail.com>
2025-09-30 10:02:24 +0000

    refactor: update ESLint rules, clean up App component structure and setup working enviroment with node v20.19.5

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-import './index.css'
+import './index.css';
 
 function App() {
-
   return (
-      <div className='min-h-screen bg-gray-50'>
-        {/* Header básico */}
-        <header className='bg-white shadow-md'>
-          <div className="container mx-auto px-4 py-4">
-            <h1 className="text-xl font-bold text-blue-800">Servicios Eléctricos Testing</h1>
-          </div>
-        </header>
-      </div>
+    <div>
+      <h1 style={{ padding: '2rem', textAlign: 'center' }}>
+        ElectroServicios - ¡Funciona!
+      </h1>
+    </div>
   );
 }
-            
 
-export default App
+export default App;
