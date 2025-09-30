8104d716a3c22020c5ca4cbd4a5d7e2260413cc2
ThebellepoquE <thebellepoque@gmail.com>
2025-09-30 10:20:03 +0200

    Remove tailwind dependencies and setup custom CSS, Setup basic container and header styles, Decision to use pure Css due to Tailwind configuration issues and clean package.json from taiwind dependencies.

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import './App.css'
+import './index.css'
 
 function App() {
 
   return (
-    <>
-      <div>
-
-        <h1>Hello Vite!</h1>
-
+      <div className='min-h-screen bg-gray-50'>
+        {/* Header básico */}
+        <header className='bg-white shadow-md'>
+          <div className="container mx-auto px-4 py-4">
+            <h1 className="text-xl font-bold text-blue-800">Servicios Eléctricos Testing</h1>
+          </div>
+        </header>
       </div>
-    </>
-  )
+  );
 }
+            
 
 export default App
