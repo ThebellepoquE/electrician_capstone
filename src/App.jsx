1c9aa3b405def8838edc8d934f84f5f3bd83830c
IonE <thebellepoque@gmail.com>
2025-09-30 16:32:43 +0000

    feat: update file structure and styles, replace CSS with SCSS, add logo and contact button

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import Home from './pages/Home.jsx';
 import Servicios from './pages/Servicios.jsx';
 import AcercaDe from './pages/AcercaDe.jsx';
 import Contacto from './pages/Contacto.jsx';
-import './index.css';
+import './index.scss';
+import logo from './assets/logo.png';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
@@ -11,13 +12,20 @@ function App() {
       <div className='App'>
         <div className='header-wrapper'>
           <header className='header'>
-            <div className='logo'>LOGO</div>
-            <nav className='navigation'>
-              <Link to='/'>INICIO</Link>
-              <Link to='/servicios'>SERVICIOS</Link>
-              <Link to='/acerca-de'>ACERCA DE</Link>
-              <Link to='/contacto'>CONTACTO</Link>
-            </nav>
+            <div className='logo-section'>
+              <img src={logo} alt="Logo de la app" className="logo" />
+            </div>
+              <div className='nav-wrapper'>
+                <nav className='navigation'>
+                  <Link to='/'>INICIO</Link>
+                  <Link to='/servicios'>SERVICIOS</Link>
+                  <Link to='/acerca-de'>ACERCA DE</Link>
+                  <Link to='/contacto'>CONTACTO</Link>
+                </nav>
+                <div className='cta-wrapper'>
+                  <a href='tel:+34123456789' className='cta-btn'>Llama al 123 456 789</a>
+                </div>
+              </div>
           </header>
         </div>
         <Routes>
