9b7ba316f1e9028841e6a19ac9c2ce60c83a0f12
thebellepoque <thebellepoque@gmail.com>
2025-10-14 19:46:17 +0200

    chore: configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default {
+export default defineConfig({
   plugins: [react()],
+  base: '/electrician_capstone/',
   server: {
     host: '127.0.0.1',
     port: 5000,
@@ -10,5 +12,5 @@ export default {
       '/api': 'http://localhost:3001'
     }
   }
-}
+})
 
